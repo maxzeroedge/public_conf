24bb783f743053678da4014cd252c87f735d9bb8
Palash <maxzeroedge@gmail.com>
2023-07-02 17:58:24 +0530

    fixed build script

--- vim_builder.sh
+++ vim_builder.sh
@@ -5,11 +5,11 @@ cp nvim_init.vim $HOME/.config/nvim/init.vim
 cd $HOME/apps
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-echo "$(cat .env_extras)alias nvim=\"$HOME/apps/nvim.appimage\"\n" >> .env_extras
+echo "$(cat $HOME/.env_extras)alias nvim=\"$HOME/apps/nvim.appimage\"\n" >> $HOME/.env_extras
 alias nvim="$HOME/apps/nvim.appimage"
 mkdir -p $HOME/.local/share/nvim/site/autoload
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +UpdateRemotePlugins +qa
-sed -i.bu 's/async=True/**{"async": True}/' ~/.config/nvim/plugged/nvim-completion-manager/pythonx/cm.py
-sed -i.bu 's/async=True/**{"async": True}/' ~/.config/nvim/plugged/nvim-completion-manager/pythonx/cm_core.py
+sed -i.bu 's/async=True/**{"async": True}/' ~/.local/share/nvim/plugged/nvim-completion-manager/pythonx/cm.py
+sed -i.bu 's/async=True/**{"async": True}/' ~/.local/share/nvim/plugged/nvim-completion-manager/pythonx/cm_core.py
 
