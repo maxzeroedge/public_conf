51d2b3573b79351a6e488d154b18bc965472ca4e
Palash <pc@pop-os.localdomain>
2023-07-09 19:06:43 +0530

    Minor fix

--- vim_builder.sh
+++ vim_builder.sh
@@ -5,7 +5,7 @@ cp nvim_init.vim $HOME/.config/nvim/init.vim
 cd $HOME/apps
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-echo "$(cat $HOME/.env_extras)alias nvim=\"$HOME/apps/nvim.appimage\"\n" >> $HOME/.env_extras
+echo "$(cat $HOME/.env_extras)\nalias nvim=\"$HOME/apps/nvim.appimage\"\n" >> $HOME/.env_extras
 alias nvim="$HOME/apps/nvim.appimage"
 mkdir -p $HOME/.local/share/nvim/site/autoload
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
